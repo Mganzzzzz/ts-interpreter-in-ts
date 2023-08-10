3610b8207682a7df393d3f7590f15e5591e28dbf
hongyuue <hongyuue@163.com>
2023-08-10 14:22:29 +0800

    feat: toString 和 log工具函数

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,13 @@ import * as path from 'path'
 import {Tokenizer} from "./tokenizer";
 import {AstParser} from "./ast-parser";
 import {ProgramExec} from "./program-exec";
+import { log } from './utils/help';
 
 function main() {
     const s = fs.readFileSync(path.join(path.dirname(__filename), './source1.txt'), {encoding: "utf-8"})
     const tokenizer = new Tokenizer(s)
     let tokens = tokenizer.parse()
-    console.log('debug tokens', tokens)
+    log('debug tokens', tokens)
     const ast = new AstParser(tokens)
     const astTree = ast.parseProgram()
     const programExec = new ProgramExec(astTree)
